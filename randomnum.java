b7cb7ca8af47505ac0a0c74da859f58d5e2bdef8
Haritharagupathi <haritharagupathi328@gmail.com>
2025-01-17 12:56:16 +0530

    Add files via upload

---
+++ randomnum.java
@@ -0,0 +1,29 @@
+public class randomnum {
+    public static void main(String[] args){
+        int[][] array=new int[100][10];
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+            array[i][j]=randomNumber();
+        }
+    }
+    print2Darray(array);
+}
+
+public static int randomNumber(){
+    double rand=Math.random()*100;
+    return (int)rand;
+}
+
+public static void print2Darray(int[][] array){
+    for(int i=0;i<array.length;i++){
+        for(int j=0;j<array[i].length;j++){
+            System.out.print(array[i][j]+" ");
+        }
+        System.out.println("\n");
+    }
+}
+}
+
+
+
+
